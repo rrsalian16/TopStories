af39aa1febfc33277ed3e97b3d320d50c60ffa75
rrsalian16 <rrsalian16@gmail.com>
2023-07-05 21:06:12 +0400

    feat: add husky and absolute import

---
+++ src/Setup/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const Setup: React.FC = () => {
+    return (
+        <View>
+            <Text>Top Stories</Text>
+        </View>
+    );
+};
+
+export default Setup;
